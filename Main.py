63006875dfd70dff5f4f34de56a81e469358c4da
LucasFernandes2006 <lucasdasilvafernandes27@gmail.com>
2025-04-21 18:09:15 -0300

    ✨:feat Login e Logout

--- Main.py
+++ Main.py
@@ -1,21 +1,30 @@
-
-from flask  import Flask, render_template, request, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask  import Flask, render_template, request, redirect, flash, url_for
 #permissão: pip install flask-sqlalchemy
-
+from flask_sqlalchemy import SQLAlchemy
+# biblioteca responsável pelo gerenciamneto do login (precisa do pip install flask-login ) 
+from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
 
 # a linha abaixo inicia a variavel de aplicação
 #Se não colocar URI não tem como conectar o banco em pg html
 app = Flask(__name__)
+
+#permite o flask_login funcionar corretamente 
+app.secret_key="MedDay"
+
+#Variável que permite o acesso ao flask_login atravéz da variável de aplicação utilizada como parâmetro 
+lm = LoginManager(app)
+
+#faz com que caso o usuário não esteja logado seja direcionado autoamticamente para login
+lm.login_view = '/login'
+
 app.config['SQLALCHEMY_DATABASE_URI']  = \
     'mysql+pymysql://root:Lucas_62@localhost:3306/projeto_semestral'
 
 #a linha abaixo instancia o banco de dados
 db = SQLAlchemy(app)
 
-#cometario de teste
-
-class Cadastro_paciente(db.Model):
+#UserMixin permite que o flask_login reconhecer a seguinte classe de usuário
+class Cadastro_paciente(UserMixin, db.Model):
     nome = db.Column(db.String(200), nullable = False)
     cpf = db.Column(db.String(11), primary_key =True)
     email = db.Column(db.String(200),  nullable=False)
@@ -28,10 +37,16 @@ class Cadastro_paciente(db.Model):
     cidade = db.Column(db.String(200), nullable=True)
     UF = db.Column(db.String(2), nullable=False)
 
+    #O UserMixin por padrão espera que exista um campo chamado id para usar como identificador,
+    #mas já que cpf é o indentificador é necessáio usar a seguinte função para mudar o identificador padrão 
+    def get_id(self):
+        return str(self.cpf)
 
-@app.route("/login")
-def logar():
-    return render_template("./login.html")
+#variável que carrega o usuário atravez da sua busca do seu cpf
+@lm.user_loader
+def user_loader(cpf):
+    usuario = db.session.query(Cadastro_paciente).filter_by(cpf = cpf).first()
+    return usuario
 
 @app.route("/cadastrar")
 def cadastrar_usuario():
@@ -41,6 +56,19 @@ def cadastrar_usuario():
 def resetar_senha():
     return  render_template("./resetarsenha.html")
 
+@app.route("/")
+#faz com que essa rota só possa ser acessada se estiver logado
+@login_required
+def home():
+    return render_template("./home.html", nome = current_user.nome, email = current_user.email)
+
+@app.route('/logout')
+@login_required
+def logout():
+    #realiza o logout do usuário
+    logout_user()
+    return redirect(url_for('home'))
+
 @app.route("/add", methods = ['POST'])
 def add_banco():
     nome_input = request.form['nome']
@@ -49,6 +77,7 @@ def add_banco():
     data_input = request.form['datanasc']
     tel_input = request.form['telefone']
     senha_input = request.form['senha']
+    validsenha_input = request.form['validsenha']
     cep_input = request.form['cep']
     rua_input = request.form['rua']
     bairro_input = request.form['bairro']
@@ -57,20 +86,26 @@ def add_banco():
         
     novo_usuario = Cadastro_paciente(nome = nome_input, cpf = cpf_input, data_nasc = data_input,
             email = email_input, senha = senha_input, telefone = tel_input, cep = cep_input, rua =  rua_input,
-            bairro = bairro_input, cidade = cidade_input, UF = estado_input  )
+            bairro = bairro_input, cidade = cidade_input, UF = estado_input)
 
     user = db.session.query(Cadastro_paciente).filter_by(cpf = cpf_input ).first()
     if user:
         alert = True
+        alert_txt = "Esse CPF já foi cadastrado"
     else:
-        alert = False
-        #a linha abaixo adiciona os dados para verificação da entrada de dados
-        db.session.add(novo_usuario)
+        if senha_input != validsenha_input:
+            alert = True
+            alert_txt = "As senhas não coincidem"
+        else:    
+            alert = False
+            alert_txt = ""
+            #a linha abaixo adiciona os dados para verificação da entrada de dados
+            db.session.add(novo_usuario)
 
-        #a linha abaixo grava as alterações no banco de dados
-        db.session.commit()
+            #a linha abaixo grava as alterações no banco de dados
+            db.session.commit()
 
-    return render_template("./cadastrar.html", alert_value = alert)
+    return render_template("./cadastrar.html", alert_value = alert, txt_alert = alert_txt)
 
 @app.route("/reset_password", methods = ['POST'])
 def reset_password():
@@ -91,25 +126,31 @@ def reset_password():
                 alert_txt = '' 
                 user.senha = new
                 #a linha abaixo grava as alterações no banco de dados
-                db.session.commit()
-            
+                db.session.commit()          
         else:
             alert = True
             alert_txt = 'Senha antiga inválida'     
     else:
         alert = True
         alert_txt = 'CPF não válido' 
-
-    """
-    cpf_input = request.form['cpf']
-    user = db.session.query(Cadastro_paciente).filter_by(cpf = cpf_input ).first()
-
-    new = request.form['senha_nova']
-    user.senha = new
-    #a linha abaixo grava as alterações no banco de dados
-    db.session.commit()
-    """
     return render_template("./resetarsenha.html",alert_value = alert, txt_alert = alert_txt)
 
+@app.route("/login", methods = ['GET','POST'])
+def logar():
+    #se o método da requisição for GET: 
+    if request.method == 'GET':
+        return render_template('./login.html')
+    #se o método da requisição for POST: 
+    elif request.method == 'POST':
+        email_input = request.form['email']
+        senha_input = request.form['senha']
+        user = db.session.query(Cadastro_paciente).filter_by(email = email_input, senha = senha_input).first()
+        if not user:
+            return "Email ou senha incorretos."
+        else:
+            #realiza o login do usuário
+            login_user(user)
+            #redireciona para home depois do login
+            return redirect(url_for('home')) 
 
 app.run(debug=True)
--- static/script/cadastro.js
+++ static/script/cadastro.js
@@ -1,21 +1,5 @@
-document.getElementById ("btn_cadastrar").addEventListener ("click", validarSenhas, false);
-
 document.getElementById ("btn_dispose").addEventListener ("click", disposeDiv, false);
 div_erro = document.getElementById("erro_div")
-
-function validarSenhas() {
-    const senha = document.getElementById("senha").value;
-    const confirmar = document.getElementById("validSenha").value;
-    const erro = document.getElementById("mensagemErro");
-
-    if (senha !== confirmar) {
-    erro.textContent = "As senhas não coincidem.";
-      return false; // impede o envio do formulário
-    }
-
-    erro.textContent = ""; // limpa a mensagem se estiver tudo certo
-    return true; // permite o envio do formulário
-}
 
 function disposeDiv(){
     div_erro.style.display = 'none';
