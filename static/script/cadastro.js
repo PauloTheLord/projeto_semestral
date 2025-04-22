772769b522f62d2596b8dd703bff309f5c0fbf2e
PauloTheLord <pcpaulocezar13@gmail.com>
2025-04-22 16:15:11 -0300

    ✨ feat: preencher CEP automaticamente:
    - verificar se o CEP é válido;
    - puxar o JSON do endereço com base no CEP digitado (ViaCEP.com.br)

--- static/script/cadastro.js
+++ static/script/cadastro.js
@@ -24,6 +24,47 @@ function prevStep() {
 
 showStep(stepAtual);
 
+// Preenchimento automatico do CEP (ViaCEP)
+
+const cep = document.querySelector("#cep");
+const rua = document.querySelector("#rua");
+const bairro = document.querySelector("#bairro");
+const cidade = document.querySelector("#cidade");
+const message_error_cep = document.querySelector("#message_error_cep")
+
+cep.addEventListener('focusout', async () => {
+
+  try {
+    const onlyNumbers = /^[0-9]+$/;
+    const cepValid = /^[0-9]{8}$/;
+
+    if(!onlyNumbers.test(cep.value) || !cepValid.test(cep.value)) {
+      throw {cep_error: 'Cep invalid' };
+    }
+
+    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+
+    if(!response.ok) {
+      throw await response.json();
+    }
+
+    const responseCep = await response.json();
+
+    rua.value = responseCep.logradouro;
+    bairro.value = responseCep.bairro;
+    cidade.value = responseCep.localidade;
+
+  } catch (error) {
+    if(error?.cep_error) {
+      message_error_cep.textContent = error.cep_error;
+      setTimeout(() => {
+        message_error_cep.textContent = '';
+      }, 5000);
+    }
+    console.log(error)
+  }
+});
+
 // Validação das senhas
 
 document.getElementById ("btn_dispose").addEventListener ("click", disposeDiv, false);
